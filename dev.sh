d75572bd576380e289d19cc286db995d464b6504
GUO YANKE <hi@gyk.me>
2025-09-16 11:30:51 +0800

    chore(dev.sh): streamline Docker build and push into single command
    Replaced separate `docker build` and `docker push` commands with a single `docker build --push` to reduce redundancy and improve efficiency in the image deployment workflow. [skip ci]

--- dev.sh
+++ dev.sh
@@ -8,6 +8,4 @@ IMAGE_TAG="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)"
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o loki-cos-restore .
 
-docker build --platform linux/amd64 -t "${PRIVATE_IMAGE}:${IMAGE_TAG}" .
-
-docker push --platform linux/amd64 "${PRIVATE_IMAGE}:${IMAGE_TAG}"
+docker build --platform linux/amd64 --push -t "${PRIVATE_IMAGE}:${IMAGE_TAG}" .
